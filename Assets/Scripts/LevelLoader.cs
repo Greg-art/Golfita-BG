f4222453db4de0a4df0808a94de90d48d01194fa
Grégori Sabel <seriosabel57@gmail.com>
2020-12-20 12:27:28 -0300

    LevelLoader
    criando menu e e transição

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+
+    public void ChangeScene(){
+        StartCoroutine( LoadLevel("Fase Teste")) ;
+    }
+
+    IEnumerator LoadLevel(string levelName){
+
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(levelName);
+
+
+    }
+}
