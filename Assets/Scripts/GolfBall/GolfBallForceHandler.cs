ee392077b5c168e22ff1ee34e11b4fd6c9d51443
Grégori Sabel <seriosabel57@gmail.com>
2020-12-22 21:55:48 -0300

    correção do mouse 2.0

--- Assets/Scripts/GolfBall/GolfBallForceHandler.cs
+++ Assets/Scripts/GolfBall/GolfBallForceHandler.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GolfBallForceHandler : MonoBehaviour
 {
-    [SerializeField] private float _maxForceX, _maxForceY = 10f;
-    [SerializeField] private float _forceMultiplier = 5f;
+    [SerializeField] private float _maxForceX, _maxForceY = 250f;
+    [SerializeField] private float _forceMultiplier = 10f;
     private float _forceX, _forceY = 10f;
     private Rigidbody _rigidbody;
 
@@ -21,8 +21,14 @@ public class GolfBallForceHandler : MonoBehaviour
 
         if (newX > _maxForceX)
             _forceX = _maxForceX;
+        else if(newX < (_maxForceX * -1))
+            _forceX = _maxForceX * -1;
+
         if (newY > _maxForceY)
             _forceY = _maxForceY;
+        else if(newY < (_maxForceY * -1))
+            _forceY = _maxForceY;
+        
     }
 
     public void ApplyForce()
