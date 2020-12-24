05c33ca2f23aaf0d8bbfac6135b6653a2cadf685
Grégori Sabel <seriosabel57@gmail.com>
2020-12-24 16:24:56 -0300

    só falta clicar

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,20 +6,36 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] private UIAnimator _uiAnimator;
+    private bool _animationEnded = false;
     public Animator transition;
 
+    void update(){
+        if (_animationEnded) {
+            Debug.Log("ended");
+            if (Input.GetKey(KeyCode.A) || (Input.GetTouch(0).phase == TouchPhase.Began)){
+                Debug.Log("ended2");
 
+                ChangeScene();
+            }
+        }
+    }
+    public void passSheStage(){ 
+        _uiAnimator.playAnimation();
+        _animationEnded = true;
+
+    }
     public void ChangeScene(){
-        StartCoroutine( LoadLevel("Fase Teste")) ;
+        StartCoroutine( LoadLevel()) ;
     }
 
-    IEnumerator LoadLevel(string levelName){
+    IEnumerator LoadLevel(){
 
         transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(1);
 
-        SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
 
     }
