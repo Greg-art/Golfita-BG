e54c71bd46eb54fe3839f00018f0e9d2bafa265b
Grégori Sabel <seriosabel57@gmail.com>
2020-12-28 22:07:29 -0300

    Destroyer

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    [SerializeField] private LevelLoader _levelLoader = default;
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("trigger");
+        if (other.GetComponent<GolfBallShotHandler>() != null)
+            Debug.Log("trigger2");
+            _levelLoader.RestartScene();
+
+    }
+
+}
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -28,6 +28,18 @@ public class LevelLoader : MonoBehaviour
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
+    }
+
+    public void RestartScene(){ 
+        StartCoroutine(ResetScene());
+    }
+    IEnumerator ResetScene(){
+
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
     }
 }
