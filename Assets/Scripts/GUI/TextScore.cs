b91bc8ffec0f392091153c89fc46d5b7e42577e7
Bernardo Mesko <bemesko@gmail.com>
2020-12-30 22:46:16 -0300

    Cria script para o texto da pontuação

---
+++ Assets/Scripts/GUI/TextScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextScore : MonoBehaviour
+{
+    private TextMeshProUGUI _scoreText;
+
+    private void Awake()
+    {
+        _scoreText = GetComponent<TextMeshProUGUI>();
+        _scoreText.SetText($"Pontos: {ScoreManager.Instance.TotalScore}");
+    }
+}
