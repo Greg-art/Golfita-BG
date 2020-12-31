995dfdc4f8eda5b58e7a926a6bddf35f4e21e4c9
Bernardo Mesko <bemesko@gmail.com>
2020-12-30 22:49:50 -0300

    Cria script para o texto de high score

---
+++ Assets/Scripts/GUI/TextHighScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextHighScore : MonoBehaviour
+{
+    private TextMeshProUGUI _highScoreText;
+
+    private void Awake()
+    {
+        _highScoreText = GetComponent<TextMeshProUGUI>();
+        _highScoreText.SetText($"Recorde: {PlayerPrefs.GetInt("highScore", 0)}");
+    }
+}
